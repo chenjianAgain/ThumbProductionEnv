d30dcf6795f8dde86a41d9bf10252404fa773aee
joseph <joseph@chenjian.local>
2015-01-03 13:57:43 +0800

    Initial Commit

---
+++ ThumbProductionEnvironment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThumbProductionEnvironment
+//
+//  Created by joseph on 15/1/3.
+//  Copyright (c) 2015年 joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThumbProductionEnvironment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThumbProductionEnvironment
+//
+//  Created by joseph on 15/1/3.
+//  Copyright (c) 2015年 joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
